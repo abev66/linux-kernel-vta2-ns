850c0aaff25d442d341de5ffef29dacb7bdeba8b
lRabbit <abev66@gmail.com>
2012-11-20 14:38:34 +0800

    cpufreq: s5pv210: 1248MHz

--- arch/arm/mach-s5pv210/mach-herring.c
+++ arch/arm/mach-s5pv210/mach-herring.c
@@ -463,7 +463,7 @@ static struct s5p_media_device herring_media_devs[] = {
 #ifdef CONFIG_CPU_FREQ
 static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
 	{
-		.freq	= 1260000,
+		.freq	= 1248000,
 		.varm	= 1425000,
 		.vint	= 1125000,
 	}, {
